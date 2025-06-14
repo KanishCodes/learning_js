09009f48e7cf7b526a9968f5e2aa96eb5c9d6659
Kanish <ssdnkanishmahajan@gmail.com>
2025-06-14 12:38:00 +0000

    learnt about Type Conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// Nan = not a number
+
+let isLoggedIn = 2
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "kanish" => true
+// " " => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
